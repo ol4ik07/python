659aa2ad9002f50d4effad32eb1cf0ad7284cecf
OlgaPC <olenka89@list.ru>
2018-07-29 20:32:23 +0300

    refactor code

--- test_add_group.py
+++ test_add_group.py
@@ -15,44 +15,60 @@ class test_add_group(unittest.TestCase):
         self.wd = WebDriver(capabilities={"marionette": False})
         self.wd.implicitly_wait(60)
     
-    def test_test_add_group(self):
+    def test_add_group(self):
         success = True
         wd = self.wd
-        wd.get("http://localhost/addressbook/")
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
-        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
-        wd.find_element_by_link_text("groups").click()
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.open_groups_page(wd)
+        self.create_group(wd, group_name="test_group1", header="test_group1_header", footer="test_group1_footer")
+        self.open_groups_page(wd)
+        self.logout(wd)
+    def test_empty_group(self):
+        success = True
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="", password="")
+        self.open_groups_page(wd)
+        self.create_group(wd, group_name="test_group1", header="test_group1_header", footer="test_group1_footer")
+        self.open_groups_page(wd)
+        self.logout(wd)
+    def logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
+
+    def create_group(self, wd, group_name, header, footer):
+        # init group creation
         wd.find_element_by_name("new").click()
+        # fill group form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("test_group1")
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").send_keys("\\9")
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("test_group1")
+
+        wd.find_element_by_name("group_name").send_keys(group_name)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("test_group1_heder")
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("test_group1_header")
+
+        wd.find_element_by_name("group_header").send_keys(header)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("test_group1_footer")
+        wd.find_element_by_name("group_footer").send_keys(footer)
+        # submit group creation
         wd.find_element_by_name("submit").click()
-        wd.find_element_by_id("header").click()
+
+    def open_groups_page(self, wd):
         wd.find_element_by_link_text("groups").click()
-        wd.find_element_by_xpath("//div[@id='content']/form").click()
-        wd.find_element_by_css_selector("html").click()
-        wd.find_element_by_link_text("Logout").click()
-        self.assertTrue(success)
-    
+
+    def login(self, wd, username, password):
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+
+    def open_home_page(self, wd):
+        wd.get("http://localhost/addressbook/")
+
     def tearDown(self):
         self.wd.quit()
 
