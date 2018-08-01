4f56260438321ce23959230d2605235441296474
OlgaPC <olenka89@list.ru>
2018-08-01 23:59:36 +0300

    add_contact

---
+++ contacts.py
@@ -0,0 +1,16 @@
+class contact:
+    def __init__(self, firstname, midname, lastname, nik, title, company, address, home, mobile, work,
+                    fax, email):
+        self.firstname=firstname
+        self.midname=midname
+        self.lastname=lastname
+        self.nik=nik
+        self.title=title
+        self.company=company
+        self.address=address
+        self.home=home
+        self.mobile=mobile
+        self.work=work
+        self.fax=fax
+        self.email=email
+
--- test_add_group.py
+++ test_add_group.py
@@ -2,6 +2,7 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
 from selenium.webdriver.common.action_chains import ActionChains
 import time, unittest
+from group import group
 
 def is_alert_present(wd):
     try:
@@ -21,36 +22,36 @@ class test_add_group(unittest.TestCase):
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
         self.open_groups_page(wd)
-        self.create_group(wd, group_name="test_group1", header="test_group1_header", footer="test_group1_footer")
+        self.create_group(wd, group(name="test_group1", header="test_group1_header", footer="test_group1_footer"))
         self.open_groups_page(wd)
         self.logout(wd)
     def test_empty_group(self):
         success = True
         wd = self.wd
         self.open_home_page(wd)
-        self.login(wd, username="", password="")
+        self.login(wd, username="admin", password="secret")
         self.open_groups_page(wd)
-        self.create_group(wd, group_name="test_group1", header="test_group1_header", footer="test_group1_footer")
+        self.create_group(wd, group(name="", header="", footer=""))
         self.open_groups_page(wd)
         self.logout(wd)
     def logout(self, wd):
         wd.find_element_by_link_text("Logout").click()
 
-    def create_group(self, wd, group_name, header, footer):
+    def create_group(self, wd, group):
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
 
-        wd.find_element_by_name("group_name").send_keys(group_name)
+        wd.find_element_by_name("group_name").send_keys(group.name)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
 
-        wd.find_element_by_name("group_header").send_keys(header)
+        wd.find_element_by_name("group_header").send_keys(group.header)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(footer)
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
         # submit group creation
         wd.find_element_by_name("submit").click()
 
